b6bfc5084e53efb171072d7cde2df707ba57dac4
onlykey <t@T-2.local>
2017-05-02 15:18:31 -0400

    test time

--- u2f-test.js
+++ u2f-test.js
@@ -90,7 +90,8 @@ function auth_timeset() { //OnlyKey settime to keyHandle
   msg("Authorizing user " + userId());
   messageHeader = [255, 255, 255, 255];
   messageType = 228
-  epochTime = Math.round(new Date().getTime() / 1000.0).toString(16);
+  epochTime = [89, 8, 219, 7]; //5908DB07
+  //epochTime = Math.round(new Date().getTime() / 1000.0).toString(16);
   msg("Setting current epoch time on OnlyKey to " + epochTime);
   keyHandle = messageHeader + messageType + epochTime
   var challenge = mkchallenge();
