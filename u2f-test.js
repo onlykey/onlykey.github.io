1dac652ef7f1f3dc058d20a2544d9259abdb4c5f
onlykey <t@T-2.local>
2017-05-09 01:33:01 -0400

    fix

--- u2f-test.js
+++ u2f-test.js
@@ -108,7 +108,7 @@ function auth_timeset() { //OnlyKey settime to keyHandle
   message = [255, 255, 255, 255, 228]; //Add header and message type
 
   //var epochTime = [89, 8, 219, 7]; //5908DB07
-  var currentEpochTime = string2bytes(Math.round(new Date().getTime() / 1000.0).toString(16));
+  var currentEpochTime = Math.round(new Date().getTime() / 1000.0).toString(16);
   msg("Setting current epoch time on OnlyKey to " + currentEpochTime);
 
   //var timeParts = currentEpochTime.match(/.{2}/g).map(hexStrToDec);
