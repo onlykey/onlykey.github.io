377caeb4e3e2b35330b3c33a059e8fc3b29abda9
onlykey <t@T-2.local>
2017-05-09 02:04:48 -0400

    test

--- u2f-test.js
+++ u2f-test.js
@@ -104,18 +104,17 @@ function auth_local() {
 
 function auth_timeset() { //OnlyKey settime to keyHandle
   msg("Authorizing user " + userId());
-  var message = new Uint8Array(5);
-  message = [255, 255, 255, 255, 228]; //Add header and message type
+  var message = [255, 255, 255, 255, 228]; //Add header and message type
 
   //var epochTime = [89, 8, 219, 7]; //5908DB07
   var currentEpochTime = Math.round(new Date().getTime() / 1000.0).toString(16);
   msg("Setting current epoch time on OnlyKey to " + currentEpochTime);
 
-  //var timeParts = currentEpochTime.match(/.{2}/g).map(hexStrToDec);
+  var timeParts = currentEpochTime.match(/.{2}/g).map(hexStrToDec);
 
   var empty = new Uint8Array(55).fill(0);
 
-  //Array.prototype.push.apply(message, timeParts);
+  Array.prototype.push.apply(message, timeParts);
 
   Array.prototype.push.apply(message, empty);
 
