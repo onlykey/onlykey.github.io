5a39d5cc020ed70d4b052b3fdd6be88cb514aecc
onlykey <t@T-2.local>
2017-07-20 12:12:32 -0400

    test

--- js/bundle_decrypt.js
+++ js/bundle_decrypt.js
@@ -14619,6 +14619,7 @@ _continue()
               (function(__iced_k) {
                 if (err == null) {
                   packet = esk_packets[0];
+                  console.info("paclet:", packet);
                   key_material = km.find_pgp_key_material(key_ids[index]);
                   fingerprint = key_material.get_fingerprint();
                   privk = key_material.key;
