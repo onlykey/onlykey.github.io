ac5f8ca9545972ef557e8db43dd2ef73c4479bed
Rodney Golpe <rodney+git@golpenet.com>
2017-09-13 00:47:04 -0400

    prepping to export OnlyKeyConnector

--- OnlyKey-Connector/js/messageRequests.js
+++ OnlyKey-Connector/js/messageRequests.js
@@ -1,14 +1,28 @@
 window.addEventListener('message', function (e) {
 	console.dir(e);
-	switch(e && e.data && e.data.action) {
-		case "ENCRYPT":
-			return handleEncryptMessage(e);
-			break;
-		default:
-			//swallow
+	// if (originWhitelist.includes(e.origin)) {
+	if (e.origin) {
+		switch(e && e.data && e.data.action) {
+			case "GET_CONNECTOR":
+				return new OnlyKeyConnector();
+				break;
+			default:
+				//swallow
+		}
 	}
 });
 
 function handleEncryptMessage(e) {
 	e.source.postMessage({ result: 'ENCRYPTED', data: e.data }, e.origin);	
+}
+
+const originWhitelist = [
+	'https://'
+];
+
+class OnlyKeyConnector {
+	constructor(params) {
+		this.Sign = auth_sign;
+		this.Decrypt = auth_decrypt;
+	}
 }
