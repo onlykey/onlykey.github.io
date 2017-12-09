85227d542451215ab5cfea62cc514caabbfa03f6
onlykey <t@T-2.local>
2017-12-09 16:57:10 -0500

    test

--- js/onlykey-api.js
+++ js/onlykey-api.js
@@ -218,8 +218,16 @@ function msg_polling(params = {}, cb) {
           msg("ECDH shared: " + shared);
           shared = shared.match(/.{2}/g).map(hexStrToDec);
           msg("ECDH shared: " + shared);
+          var alice_private = "77076d0a7318a57d3c16c17251b26645df4c2f87ebc0992ab177fba51db92c2a";
+          var alice_public = "8520f0098930a754748b7ddcb43ef75a0dbf3a0d26381af4eba4a98eaa9b4e6a";
+          var bob_private = "5dab087e624a8a4b79e17f8b83800ee66f3bb1292618b6fd1c2f8b27ff88e0eb";
+          var bob_public = "de9edb7d7b7dc1b4d35b61c2ece435373f8343c85b78674dadfc7e146f882b4f";
+          var alice_mult_bob = "4a5d9d5ba4ce2de1728e3bf480350f25e07e21c947d19e3376f09b3c1e161742";
 
-
+          var pub1 = curve25519.keyFromPublic(alice_public, 'hex');
+          var priv1 = curve25519.keyFromPrivate(bob_private, 'hex');
+          shared = priv1.derive(pub1.getPublic()).toString(16);
+          msg("ECDH shared: " + shared);
 
           OKversion = result[19] == 99 ? 'Color' : 'Original';
           var FWversion = bytes2string(result.slice(8, 20));
