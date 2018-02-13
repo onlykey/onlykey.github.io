c9679a33d6ef44c322d25b279db34eed8f0a0c93
onlykey <t@T-2.local>
2018-02-13 12:27:29 -0500

    test

--- build/bundle.fdfa36a4dd30c2cf6200.js
+++ build/bundle.fdfa36a4dd30c2cf6200.js
@@ -1,3 +1,75 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
 const url = __webpack_require__(172);
 const kbpgp = __webpack_require__(169);
 const request = __webpack_require__(171);
@@ -390,3 +462,7 @@ urlinputbox.onkeyup = function () {
     let rows_current = Math.trunc((urlinputbox.value.length * parseFloat(window.getComputedStyle(urlinputbox, null).getPropertyValue('font-size'))) / (urlinputbox.offsetWidth * 1.5)) + 1;
     urlinputbox.rows = (rows_current > 10) ? 10 : rows_current;
 };
+
+
+/***/ })
+/******/ ]);
--- js/onlykey-api.js
+++ js/onlykey-api.js
@@ -6,11 +6,12 @@ var appId = window.location.origin;
 var version = "U2F_V2";
 var OKversion;
 
-var p256 = new ECC('p256');
-var sha256 = function(s) { return p256.hash().update(s).digest(); };
-var BN = p256.n.constructor;  // BN = BigNumber
+var sha256 = function(s) {
+  var md = forge.md.sha256.create();
+  md.update(s);
+  return md.digest().toHex();
+};
 
-var curve25519 = new ECC('curve25519');
 var appKey;
 var appPub;
 var appPubPart;
@@ -68,112 +69,6 @@ function updateStatusFromSelection(skipBtn) {
   const val = document.action.select_one.value;
   _status = val;
   if (!skipBtn) button.textContent = val;
-}
-
-/**
- * U2F registration pure JavaScript
- */
-function enroll_local() {
-  msg("Enrolling user " + userId());
-  var challenge = mkchallenge();
-  var req = { "challenge": challenge, "appId": appId, "version": version};
-  u2f.register(appId, [req], [], function(response) {
-    var result = process_enroll_response(response);
-    msg("User " + userId() + " enroll " + (result ? "succeeded" : "failed"));
-  });
-}
-
-/**
- * U2F authentication pure JavaScript
- */
-function auth_local() {
-  msg("Authorizing user " + userId());
-  keyHandle = userDict[userId()];
-  if (!keyHandle) {
-    msg("User " + userId() + " not enrolled");
-    return;
-  }
-  msg("Sending Handlekey " + keyHandle);
-  var challenge = mkchallenge();
-  msg("Sending challenge " + challenge);
-  var req = { "challenge": challenge, "keyHandle": keyHandle,
-               "appId": appId, "version": version };
-  u2f.sign(appId, challenge, [req], function(response) {
-    var result = process_auth_response(response);
-    msg("User " + userId() + " auth " + (result ? "succeeded" : "failed"));
-  });
-    msg("Finished");
-}
-
-/**
- * Function to process U2F registration response
- * @param {Array} response
- */
-function process_enroll_response(response) {
-  var err = response['errorCode'];
-  if (err) {
-    msg("Registration failed with error code " + err);
-    console.info(errMes);
-    return false;
-  }
-  var clientData_b64 = response['clientData'];
-  var regData_b64 = response['registrationData'];
-  var clientData_str = u2f_unb64(clientData_b64);
-  var clientData = JSON.parse(clientData_str);
-  var origin = clientData['origin'];
-  if (origin != appId) {
-    msg("Registration failed.  AppId was " + origin + ", should be " + appId);
-    return false;
-  }
-  var v = string2bytes(u2f_unb64(regData_b64));
-  var u2f_pk = v.slice(1, 66);                // PK = Public Key
-  var kh_bytes = v.slice(67, 67 + v[66]);     // KH = Key Handle
-  var kh_b64 = bytes2b64(kh_bytes);
-  var cert_der = v.slice(67 + v[66]);
-  var cert_asn1 = ASN1.decode(cert_der);
-  var cert_pk_asn1 = cert_asn1.sub[0].sub[6].sub[1];
-  var cert_pk_bytes = asn1bytes(cert_pk_asn1);
-  var cert_key = p256.keyFromPublic(cert_pk_bytes.slice(3), 'der');
-  var sig = cert_der.slice(cert_asn1.length + cert_asn1.header);
-  var l = [[0], sha256(appId), sha256(clientData_str), kh_bytes, u2f_pk];
-  var v = cert_key.verify(sha256(bcat(l)), sig);
-  if (v) {
-    userDict[userId()] = kh_b64;
-    keyHandleDict[kh_b64] = u2f_pk;
-  }
-  return v;
-}
-
-/**
- * Function to process U2F sign response
- * @param {Array} response
- */
-function process_auth_response(response) {
-  console.info("Response", response);
-  var err = response['errorCode'];
-  if (err) {
-    msg("Auth failed with error code " + err);
-    console.info(errMes);
-    return false;
-  }
-  var clientData_b64 = response['clientData'];
-  var clientData_str = u2f_unb64(clientData_b64);
-  var clientData_bytes = string2bytes(clientData_str);
-  var clientData = JSON.parse(clientData_str);
-  var origin = clientData['origin'];
-  var kh = response['keyHandle'];
-  var pk = keyHandleDict[kh];
-  var key = p256.keyFromPublic(pk, 'der');
-  var sigData = string2bytes(u2f_unb64(response['signatureData']));
-  var sig = sigData.slice(5);
-  var appid = document.location.origin;
-  var m = bcat([sha256(appid), sigData.slice(0,5), sha256(clientData_bytes)]);
-  if (!key.verify(sha256(m), sig)) return false;
-  var userPresent = sigData[0];
-  var counter2 = new BN(sigData.slice(1,5)).toNumber();
-  msg("User present: " + userPresent);
-  msg("Counter: " + counter2);
-  return true;
 }
 
 /**
