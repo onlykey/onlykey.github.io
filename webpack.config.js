31cb28a9497c67b8692fa8097bae3dc08832e361
onlykey <t@T-2.local>
2018-02-13 17:01:53 -0500

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
-const MinifyPlugin = require("babel-minify-webpack-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackIncludeAssetsPlugin = require('html-webpack-include-assets-plugin');
@@ -25,8 +25,14 @@ let plugins = [
         cache: false,
         showErrors: false
     }),
-    new MinifyPlugin({
-      mangle: false
+    new UglifyJsPlugin({
+        uglifyOptions: {
+        ie8: false,
+        ecma: 8,
+        mangle: false,
+        compress: true,
+        warnings: false
+      }
     }),
     new SriPlugin({
         hashFuncNames: ['sha256', 'sha384'],
@@ -35,7 +41,7 @@ let plugins = [
 ];
 
 module.exports = {
-    entry: ['./js/app.js', './js/forge.min.js', './js/kbpgp.js', './js/onlykey-api.js', './js/u2f-api.js'],
+    entry: ['./js/app.js', './js/forge.min.js', './js/kbpgp.js', './js/onlykey-api.js', './js/u2f-api.js', './js/nacl.min.js'],
     output: {
         path: path.resolve(__dirname, (process.env.OUT_DIR) ? process.env.OUT_DIR : './build'),
         filename: 'bundle.[hash].js',
