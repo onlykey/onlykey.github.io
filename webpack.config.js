3eae8f5d0fa5805cf56597395f2db5f51b86ed27
onlykey <t@T-2.local>
2018-02-16 12:21:46 -0500

    test

--- webpack.config.js
+++ webpack.config.js
@@ -28,11 +28,13 @@ let plugins = [
     new webpack.ProvidePlugin({
       nacl: './nacl.min.js',
       forge: './forge.min.js',
+      u2f: './u2f-api.js',
       auth_sign: './onlykey-api.js',
       auth_decrypt: './onlykey-api.js',
       _status: './onlykey-api.js',
       poll_delay: './onlykey-api.js',
-      poll_type: './onlykey-api.js'
+      poll_type: './onlykey-api.js',
+      kbpgp: './kbpgp.js'
     })
 ];
 
