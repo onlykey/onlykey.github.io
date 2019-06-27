ceb772fa6a03ac84d63152e93420295c23b4de81
onlykey <t@T-2.local>
2019-06-27 14:17:17 -0400

    test

--- src/kbpgp.js
+++ src/kbpgp.js
@@ -20126,9 +20126,9 @@ _break()
                     sig = arguments[0].to_mpi_buffer();
                     console.info("signature from app:", sig);
                     size = (ok_sig.length - 1) * 8 + nbits(ok_sig[0]);
-                    hdr = new Buffer(2);
+                    hdr = Buffer.alloc(2);
                     hdr.writeUInt16BE(size, 0);
-                    sig = Buffer.concat([hdr, new Buffer(ok_sig)]);
+                    sig = Buffer.concat([hdr, Buffer.from(ok_sig)]);
                     console.info("sig:", sig);
                     return cb(null, sig);
                 });
