25dddff070466643134461915d88668d563f1162
onlykey <t@T-2.local>
2019-06-26 22:10:42 -0400

    test

--- src/onlykey-api.js
+++ src/onlykey-api.js
@@ -525,10 +525,10 @@ function decode_ctaphid_response_from_signature(response) {
 
     if (error_code == 0) {
         data = signature.slice(1, signature.length);
-        if (signature.length<=65 && bytes2string(data.slice(0,9))=='UNLOCKEDv') {
+        if (signature.length<=64 && bytes2string(data.slice(0,9))=='UNLOCKEDv') {
           // Reset shared secret and start over
           _setStatus(document.getElementById('onlykey_start').value);
-        } else if (signature.length<=65) {
+        } else if (signature.length<=64 && bytes2string(data.slice(0,6))=='ERROR ') {
           // Something went wrong, read the ascii response and display to user
           const btmsg = `${bytes2string(data.slice(0,63))}. Refresh this page and try again.`;
           button.textContent = btmsg;
