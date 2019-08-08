99fe61a0087e480ea24b68ef7bf30a2f0a1eed40
onlykey <t@T-2.local>
2019-08-08 16:41:27 -0400

    test

--- src/onlykey-api.js
+++ src/onlykey-api.js
@@ -570,7 +570,7 @@ async function ctaphid_via_webauthn(cmd, opt1, opt2, opt3, data, timeout) {
     });
   } else {
     challenge = mkchallenge();
-    var b64keyhandle = bytes2b64(keyHandle);
+    var b64keyhandle = bytes2b64(keyhandle);
     var req = { "challenge": challenge, "keyHandle": b64keyhandle,
                "appId": appId, "version": "U2F_V2" };
 
