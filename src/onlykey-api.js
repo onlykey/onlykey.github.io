6ddbe0426a5cb863944c8f9ad8fc7182745a73de
onlykey <t@T-2.local>
2019-08-09 13:49:04 -0400

    test

--- src/onlykey-api.js
+++ src/onlykey-api.js
@@ -542,12 +542,13 @@ async function ctaphid_via_webauthn(cmd, opt1, opt2, opt3, data, timeout) {
           type: 'public-key',
       }],
       timeout: timeout,
+      rpId: 'crp.to',
       userVerification: 'discouraged',
-      userPresence: 'false',
-      mediation: 'silent',
-      extensions: {
-        appid: appId,
-      },
+      //userPresence: 'false',
+      //mediation: 'silent',
+      //extensions: {
+        //appid: appId,
+      //},
   }
 
 
