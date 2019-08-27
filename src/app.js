3b458014654d475ebabad9871b098290c160374b
onlykey <t@T-2.local>
2019-08-27 11:55:56 -0400

    test

--- src/app.js
+++ src/app.js
@@ -134,16 +134,6 @@ window.initok = initok();
 window.custom_keyid;
 
 window.initapp = function(skipBtn) {
-  if (document.action.select_one.value=='Encrypt and Sign') {
-    if (getAllUrlParams().type=='e') document.getElementById('encrypt_and_sign').checked = true;
-    if (getAllUrlParams().type=='es') document.getElementById('encrypt_only').checked = true;
-    if (getAllUrlParams().type=='s') document.getElementById('sign_only').checked = true;
-  } else if (document.action.select_one.value=='Decrypt and Verify') {
-    if (getAllUrlParams().type=='dv') document.getElementById('decrypt_and_verify').checked = true;
-    if (getAllUrlParams().type=='d') document.getElementById('decrypt_only').checked = true;
-  }
-  if (getAllUrlParams().sender) urlinputbox.value = getAllUrlParams().sender;
-  if (getAllUrlParams().recipients) urlinputbox2.value = getAllUrlParams().recipients;
   const val = document.action.select_one.value;
   window._status = val;
   if (!skipBtn) button.textContent = val;
@@ -686,56 +676,3 @@ urlinputbox.onkeyup = function () {
     let rows_current = Math.trunc((urlinputbox.value.length * parseFloat(window.getComputedStyle(urlinputbox, null).getPropertyValue('font-size'))) / (urlinputbox.offsetWidth * 1.5)) + 1;
     urlinputbox.rows = (rows_current > 10) ? 10 : rows_current;
 };
-
-function getAllUrlParams(url) {
-  // get query string from url (optional) or window
-  var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
-  // we'll store the parameters here
-  var obj = {};
-  // if query string exists
-  if (queryString) {
-    // stuff after # is not part of query string, so get rid of it
-    queryString = queryString.split('#')[0];
-    // split our query string into its component parts
-    var arr = queryString.split('&');
-    for (var i = 0; i < arr.length; i++) {
-      // separate the keys and the values
-      var a = arr[i].split('=');
-      // set parameter name and value (use 'true' if empty)
-      var paramName = a[0];
-      var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
-      // (optional) keep case consistent
-      paramName = paramName.toLowerCase();
-      if (typeof paramValue === 'string') paramValue = paramValue.toLowerCase();
-      // if the paramName ends with square brackets, e.g. colors[] or colors[2]
-      if (paramName.match(/\[(\d+)?\]$/)) {
-        // create key if it doesn't exist
-        var key = paramName.replace(/\[(\d+)?\]/, '');
-        if (!obj[key]) obj[key] = [];
-        // if it's an indexed array e.g. colors[2]
-        if (paramName.match(/\[\d+\]$/)) {
-          // get the index value and add the entry at the appropriate position
-          var index = /\[(\d+)\]/.exec(paramName)[1];
-          obj[key][index] = paramValue;
-        } else {
-          // otherwise add the value to the end of the array
-          obj[key].push(paramValue);
-        }
-      } else {
-        // we're dealing with a string
-        if (!obj[paramName]) {
-          // if it doesn't exist, create property
-          obj[paramName] = paramValue;
-        } else if (obj[paramName] && typeof obj[paramName] === 'string'){
-          // if property does exist and it's a string, convert it to an array
-          obj[paramName] = [obj[paramName]];
-          obj[paramName].push(paramValue);
-        } else {
-          // otherwise add the property
-          obj[paramName].push(paramValue);
-        }
-      }
-    }
-  }
-  return obj;
-}
--- src/onlykey-api.js
+++ src/onlykey-api.js
@@ -45,8 +45,17 @@ initok = async function () {
   //Initialize OnlyKey
     if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1 ) browser = "firefox";
     if (navigator.userAgent.toLowerCase().indexOf('android') > -1 ) browser = "android";
-    //Initialize App
-    window.initapp();
+
+    if (document.action.select_one.value=='Encrypt and Sign') {
+      if (getAllUrlParams().type=='e') document.getElementById('encrypt_and_sign').checked = true;
+      if (getAllUrlParams().type=='es') document.getElementById('encrypt_only').checked = true;
+      if (getAllUrlParams().type=='s') document.getElementById('sign_only').checked = true;
+    } else if (document.action.select_one.value=='Decrypt and Verify') {
+      if (getAllUrlParams().type=='dv') document.getElementById('decrypt_and_verify').checked = true;
+      if (getAllUrlParams().type=='d') document.getElementById('decrypt_only').checked = true;
+    }
+    if (getAllUrlParams().sender) document.getElementById('pgpkeyurl').value = getAllUrlParams().sender;
+    if (getAllUrlParams().recipients) document.getElementById('pgpkeyurl2').value = getAllUrlParams().recipients;
     if (window._status != 'Encrypt Only') {
       msg_polling({ type: 1, delay: 0 }); //Set time on OnlyKey, get firmware version, get ecc public
       await wait(6000);
@@ -54,6 +63,8 @@ initok = async function () {
         if (browser=='firefox') headermsg("OnlyKey not connected! Close this tab and open a new one to try again.");
         else headermsg("OnlyKey not connected! Refresh this page to try again.");
       } else {
+        //Initialize App
+        window.initapp();
       }
   }
 };
@@ -143,6 +154,59 @@ function bytes2string(bytes) {
 
 function bytes2b64(bytes) {
   return u2f_b64(bytes2string(bytes));
+}
+
+function getAllUrlParams(url) {
+  // get query string from url (optional) or window
+  var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+  // we'll store the parameters here
+  var obj = {};
+  // if query string exists
+  if (queryString) {
+    // stuff after # is not part of query string, so get rid of it
+    queryString = queryString.split('#')[0];
+    // split our query string into its component parts
+    var arr = queryString.split('&');
+    for (var i = 0; i < arr.length; i++) {
+      // separate the keys and the values
+      var a = arr[i].split('=');
+      // set parameter name and value (use 'true' if empty)
+      var paramName = a[0];
+      var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
+      // (optional) keep case consistent
+      paramName = paramName.toLowerCase();
+      if (typeof paramValue === 'string') paramValue = paramValue.toLowerCase();
+      // if the paramName ends with square brackets, e.g. colors[] or colors[2]
+      if (paramName.match(/\[(\d+)?\]$/)) {
+        // create key if it doesn't exist
+        var key = paramName.replace(/\[(\d+)?\]/, '');
+        if (!obj[key]) obj[key] = [];
+        // if it's an indexed array e.g. colors[2]
+        if (paramName.match(/\[\d+\]$/)) {
+          // get the index value and add the entry at the appropriate position
+          var index = /\[(\d+)\]/.exec(paramName)[1];
+          obj[key][index] = paramValue;
+        } else {
+          // otherwise add the value to the end of the array
+          obj[key].push(paramValue);
+        }
+      } else {
+        // we're dealing with a string
+        if (!obj[paramName]) {
+          // if it doesn't exist, create property
+          obj[paramName] = paramValue;
+        } else if (obj[paramName] && typeof obj[paramName] === 'string'){
+          // if property does exist and it's a string, convert it to an array
+          obj[paramName] = [obj[paramName]];
+          obj[paramName].push(paramValue);
+        } else {
+          // otherwise add the property
+          obj[paramName].push(paramValue);
+        }
+      }
+    }
+  }
+  return obj;
 }
 
 /**
