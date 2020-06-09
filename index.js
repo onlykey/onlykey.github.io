b5ac08fae44bd1357360face6bfecdebb7548c3c
EC2 Default User <ec2-user@ip-172-31-63-244.ec2.internal>
2020-06-09 14:26:41 +0000

    update

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ var app = express();
 var http = require('http');
 var https = require('https');
 var server = http.createServer(app);
-var dirname = __dirname + "/dev-new";
+var dirname = __dirname + "/current";
+const fs = require('fs')
 
 app.use((req, res, next) => {
   var u = req.url.split("?")[0]
-  switch(u){
+  switch (u) {
     case "/":
-      req.url = "/index-dev.html";
+      req.url = getFile("/index");
       break;
     case "/search":
     case "/encrypt":
@@ -17,36 +18,64 @@ app.use((req, res, next) => {
     case "/encrypt-file":
     case "/decrypt-file":
     case "/password-generator":
-      req.url = u+"-dev.html";//+req.url.split("?")[1];
+      req.url = getFile(u); //+req.url.split("?")[1];
       break;
   }
-    
-      next();
+  
+  next();
 });
+
+function getFile(filePath,prev){//   /filename =  /filename || /filename.html || /filename-dev.html
+  try {
+    if (fs.existsSync(dirname+filePath)) {
+      return filePath;
+    }else throw new Error("Not Found");
+  }
+  catch (e) {
+    try {
+      if (fs.existsSync(dirname+filePath+".html")) {
+        return filePath+".html";
+      }else throw new Error("Not Found");
+    }
+    catch (e) {
+      try {
+        if (fs.existsSync(dirname+filePath+"-dev.html")) {
+          return filePath+"-dev.html";
+        }else throw new Error("Not Found");
+      }
+      catch (e) { 
+        if(!prev)
+          return getFile("/index", filePath);
+        else return prev;
+      }
+    }
+  }
+  
+}
 
 app.use(express.static(dirname));
 app.get('/protonmail/:action/:address_id*', (req, res, next) => {
   var address_id = req.params.address_id;
   var action = req.params.action;
-  
+
   var url;
-  switch(action){
+  switch (action) {
     case "get":
-      url = 'https://api.protonmail.ch/pks/lookup?op='+action+'&search=' + address_id;
+      url = 'https://api.protonmail.ch/pks/lookup?op=' + action + '&search=' + address_id;
       break;
     case "index":
-    url = 'https://api.protonmail.ch/pks/lookup?op='+action+'&search=' + address_id;
+      url = 'https://api.protonmail.ch/pks/lookup?op=' + action + '&search=' + address_id;
       break;
-      default:
-        return next();
+    default:
+      return next();
   }
   res.set('Content-Type', 'text/plain');
   //https://api.protonmail.ch/pks/lookup?op=index&search=bmatusiak@protonmail.com
   //https://api.protonmail.ch/pks/lookup?op=get&search=bmatusiak@protonmail.com
-  
+
   //https://api.protonmail.ch/pks/lookup?op=index&search=0x0acad543e682989d <--keyid
   //https://api.protonmail.ch/pks/lookup?op=get&search=0x0acad543e682989d <--keyid
-  
+
   //https://api.protonmail.ch/pks/lookup?op=index&search=0x0396d70ff10f3c9175a76f0a149a3dc46e2e6313
   //https://api.protonmail.ch/pks/lookup?op=get&search=0x0396d70ff10f3c9175a76f0a149a3dc46e2e6313 
   https.get(url, (resp) => {
@@ -55,7 +84,7 @@ app.get('/protonmail/:action/:address_id*', (req, res, next) => {
       data += chunk;
     });
     resp.on('end', () => {
-      if(action == "get" && data.slice(0, 10) != '-----BEGIN'){
+      if (action == "get" && data.slice(0, 10) != '-----BEGIN') {
         return res.status(404).send('Not found');
       }
       res.send(data);
