886ca3030df51013b527dc2c8dd74dccd45ac8fd
EC2 Default User <ec2-user@ip-172-31-63-244.ec2.internal>
2020-06-10 22:34:25 +0000

    update

---
+++ src/app_src/pages/page_actions/past_releases.page.js
@@ -0,0 +1,102 @@
+var init = false;
+
+
+var page = {
+
+  init: function(app, pages) {
+    init = true;
+
+    console.log("page", "init");
+
+
+    page.setup(app, pages);
+  },
+  setup: function(app, pages) {
+    if (!init)
+      page.init(app, pages);
+
+    var onlykeyApi = app.onlykeyApi;
+    var $ = app.$;
+    
+    window.fetch('../past_releases/past_releases.json')
+      .then(response => response.json())
+      .then(data => {
+
+        var current;
+        var releaseList = {},
+          rl = {};
+
+        var j;
+
+        for (j in data) {
+          if (!(j.indexOf("current") == -1)) {
+            current = data[j];
+            continue;
+          }
+          rl[j] = data[j];
+        }
+        data = rl;
+
+        for (j in data) {
+          if (j == current) {
+            continue;
+          }
+          releaseList[j] = data[j];
+        }
+
+        $('<hr>').appendTo($("#releases"));
+
+        if (current)
+          releaseView(current, data[current], true);
+
+        for (var i in releaseList) {
+          $('<hr>').appendTo($("#releases"));
+          releaseView(i, releaseList[i]);
+        }
+
+
+
+        function releaseView(releasesName, releaseData, is_current) {
+
+          var contain = $('<div>').appendTo($("#releases"));
+          if (is_current) {
+            contain.append("<h2>" + releaseData.name + " : " + releaseData.stage + releaseData.version + "(live)</h2>");
+            contain.append('<a href="../" class="yui3-button primary-button">Open ' + releaseData.stage + releaseData.version + '(live)</a>');
+          }
+          else {
+            contain.append("<h2>" + releaseData.name + " : " + releaseData.stage + releaseData.version + (is_current ? "(live)" : "") + "</h2>");
+            contain.append('<a href="../past_releases/' + releasesName + '" class="yui3-button primary-button">Open ' + releaseData.stage + releaseData.version + (is_current ? "(live)" : "") + '</a>');
+
+          }
+          if (releaseData.authors) {
+            var i;
+            for (i in releaseData.change_log) {
+              contain.append("<p>*" + releaseData.change_log[i] + "</p>");
+            }
+            if (releaseData.firmware_release_url)
+              contain.append("<p><a href='" + releaseData.firmware_release_url[i] + "'>Firmware</a></p>");
+            for (i in releaseData.authors) {
+              contain.append("<p>" + releaseData.authors[i] + "</p>");
+            }
+          }
+
+
+
+        }
+
+        var vRequest = onlykeyApi.getAllUrlParams().version;
+        if (vRequest && data[vRequest]) {
+          setTimeout(() => {
+
+            window.location = "../past_releases/" + vRequest;
+
+          }, 200);
+        }
+      });
+
+
+    console.log("page", "setup");
+  }
+};
+
+module.exports = page;
