258bae7ff549af6f74bd108d24e2089679a7f036
EC2 Default User <ec2-user@ip-172-31-63-244.ec2.internal>
2020-06-26 12:31:03 +0000

    update

--- src/plugins-devel.js
+++ src/plugins-devel.js
@@ -1,6 +1,9 @@
 
 
-console.log("WARNING LOADING DEVEL PLUGINS!  WARNING!");
+if (!(process.env.NODE_ENV === "production")) { //just in case this file gets included somehow in production
+  
+  
+console.log("WARNING! ------------- LOADING DEVEL PLUGINS! ------------- WARNING!");
   
   
 module.exports = [];
@@ -15,3 +18,8 @@ module.exports.push(require("./plugins/chat/chat.js"));
 /* for encrypted data to for onlykey devices */
 module.exports.push(require("./app_src/history.js"));
 
+}else{
+  
+  console.log("ERROR! ------------- LOADING DEVEL PLUGINS GOT INCLUDED SOMEHOW IN PRODUCTION! ------------- ERROR!");
+  throw(new Error("ERROR! ------------- LOADING DEVEL PLUGINS GOT INCLUDED SOMEHOW IN PRODUCTION! ------------- ERROR!"));
+}
