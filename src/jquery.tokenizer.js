b03d78b3835214a03c1a7c0fbb44fd190f75ec86
EC2 Default User <ec2-user@ip-172-31-63-244.ec2.internal>
2020-06-30 08:25:37 +0000

    update

--- src/jquery.tokenizer.js
+++ src/jquery.tokenizer.js
@@ -282,7 +282,9 @@ var tokenizer = function($, onAddTokenizerItem) {
                     .replace(/<\/div><div>/g, "")
                     .replace(/<br>/g, "\r\n")
                     .replace(/<\/div>/g, "")
-                    .replace(/<div>/g, "");
+                    .replace(/<div>/g, "")
+                    .replace(/<\/pre>/g, "")
+                    .replace(/<pre>/g, "");
                 this.$element.text("");
                 return value;
             },
@@ -337,7 +339,7 @@ var tokenizer = function($, onAddTokenizerItem) {
                 this.$icon = $("<svg class='remove-icon bi bi-x-circle-fill' width='1em' height='1em' viewBox='0 0 16 16' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.146-3.146a.5.5 0 0 0-.708-.708L8 7.293 4.854 4.146a.5.5 0 1 0-.708.708L7.293 8l-3.147 3.146a.5.5 0 0 0 .708.708L8 8.707l3.146 3.147a.5.5 0 0 0 .708-.708L8.707 8l3.147-3.146z'/></svg>")
                     .on("click", $.proxy(this.handleRemoveClick, this));
                 this.$element = $("<span class='label'></span>")
-                    .append(this.value.replace(/\r\n/g, "<br>"))
+                    .append(this.value/*.replace(/\r\n/g, "<br>")*/)
                     .append(this.$icon);
             },
 
