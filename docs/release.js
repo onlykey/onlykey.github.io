f6fbd6dfb767d36db782810103a2603c4cec8b6d
bmatusiak <bmatusiak@gmail.com>
2020-07-08 04:34:55 +0000

    update

---
+++ docs/release.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "name": "OnlyKey Webcrypt App",
+  "version": "2.1.0",
+  "stage": "prodc",
+  "change_log": [
+    "Added Multiple Recipient Support for GPG Encryption",
+    "Added Password Generator",
+    "~Added Build&Plugin for source"
+  ],
+  "authors":[
+    "Brad ~ bmatusiak.us",
+    "Tim ~ onlykey.io"
+  ],
+  //"firmware_release_url":"https://github.com/trustcrypto/OnlyKey-Firmware/releases"
+  "mode": process.env.NODE_ENV
+};
