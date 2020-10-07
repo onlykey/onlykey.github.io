291c2f96b96735b7a193c341e9242d8612d8dc54
bmatusiak <bmatusiak@gmail.com>
2020-10-07 06:20:07 +0000

    removed chat plugin from dev build

--- src/plugins-devel.js
+++ src/plugins-devel.js
@@ -15,7 +15,7 @@ module.exports = [];
 module.exports.push(require("./plugins/console/console_debug.js"));
 
 /* chat plugin */
-module.exports.push(require("./plugins/chat/chat.js"));
+//module.exports.push(require("./plugins/chat/chat.js"));
 
 /* for encrypted data to for onlykey devices */
 module.exports.push(require("./lib/history.js"));
